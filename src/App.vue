10e19a4373badc0ccf49061458e8e07f7379bb21
Guilherme Jansen <52773109+guilhermejansen@users.noreply.github.com>
2024-02-29 05:12:25 -0300

    main

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <iframe
+      src="https://linktr.ee/guilhermejansenoficial"
+      frameborder="0"
+      style="border:none; overflow:hidden; width:100vw; height:100vh; margin:auto; display:block;"
+      scrolling="no"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* Remove as margens padrões e define a largura e altura para ocupar toda a tela */
+html, body, #app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow: hidden; /* Evita a barra de rolagem na página do Vue */
+}
+
+iframe {
+  position: absolute; /* Permite que o iframe ocupe toda a tela */
+  top: 0;
+  left: 0;
+}
+</style>
